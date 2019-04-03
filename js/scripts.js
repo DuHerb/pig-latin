2aea007c1a8c02a95f06bb692086c6b5f0312bdf
Dustin Herboldshimer and Gavin Kimberlin <dustin@>
2019-04-03 16:36:59 -0700

    burn it all down to the ground

--- js/scripts.js
+++ js/scripts.js
@@ -26,10 +26,46 @@ $(document).ready(function(){
   };
   // console.log(testInput.charAt(0));
   // console.log(firstVowel(testInput));
+  // consecCons takes one word and looks for consecutive consonates and turns it into a pig latin word
+  function consecCons(input) {
+    var index = 0;
+    var inputLocal = input;
+    var output = "";
+    var breakOut = false;
+
+    for (var i = 0; i < vowels.length; i++) {
+      if (input.indexOf(vowels[i]) != -1) {
+        index = input.indexOf(vowels[i]);
+        breakOut = true;
+      }
+      if (breakOut) {
+        break;
+      }
+    }
+    console.log(index);
+    // vowels.some( function(vowel){
+    //   if (input.indexOf(vowel) != -1){
+    //     index = input.indexOf(vowel);
+    //       return true;
+    //   }
+    // });
+    // var inputSlice = inputLocal.slice(index);
+    // var inputSplice = inputLocal.split('').splice(0, index);
+    // output = inputSlice + inputSplice.join('') + "ay";
+    //
+    // console.log(inputSlice);
+    // console.log(inputSplice);
+    // console.log(output);
+    return index;
+  };
 
   $("#userInputSubmit").click(function(){
     var userInput = $(".userInput").val();
-    $('output').append("<br>" + singleChar(userInput));
-    $('output').append("<br>" + firstVowel(userInput));
+    // $('output').append("<br>" + singleChar(userInput));
+    // $('output').append("<br>" + firstVowel(userInput));
+    $('output').append("<br>" + consecCons(userInput));
   });
+
+//   var word = "charge";
+// console.log(word.indexOf("a"));
 });
